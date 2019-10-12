5ece2da5f27cfd916eb7aff5bb08606aca834fdd
KendallRConley <kendall.conley@uky.edu>
2019-10-11 23:00:42 -0400

    added bottom navigation bar, which switch between fragments to display

--- app/src/main/java/com/example/danceblue/MainActivity.java
+++ app/src/main/java/com/example/danceblue/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.danceblue;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new home()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch(menuItem.getItemId()){
+                        case R.id.action_home:
+                            selectedFragment = new home();
+                            break;
+                        case R.id.action_events:
+                            selectedFragment = new events();
+                            break;
+                        case R.id.action_blog:
+                            selectedFragment = new blog();
+                            break;
+                        case R.id.action_more:
+                            selectedFragment = new more();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
