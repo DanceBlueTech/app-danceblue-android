b8a5ac38e2109f79e338bb1354e01437680b9559
Adrian <adrian.carideo@uky.edu>
2019-10-13 19:30:23 -0400

    Revert "Updated project from Support libraries to AndroidX libraries to allow Firebase integration. Began sketching out the layout of the Home fragment, added a scrollable view. All images are placeholders for now"
    This reverts commit 327fc641

--- app/src/main/java/com/example/danceblue/MainActivity.java
+++ app/src/main/java/com/example/danceblue/MainActivity.java
@@ -1,13 +1,11 @@
 package com.example.danceblue;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
 
