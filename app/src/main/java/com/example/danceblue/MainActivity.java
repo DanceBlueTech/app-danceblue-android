a9ecd552b7908ee70fa5ca636d015b667a1c8a19
KendallRConley <kendall.conley@uky.edu>
2019-10-17 13:13:31 -0400

    Added code to change title of page depending on which page you are on

--- app/src/main/java/com/example/danceblue/MainActivity.java
+++ app/src/main/java/com/example/danceblue/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.danceblue;
 
 import androidx.annotation.NonNull;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +24,13 @@ public class MainActivity extends AppCompatActivity {
                 new home()).commit();
     }
 
+    private void changeToolbarText (String Title){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        TextView toolbarTitle = findViewById(R.id.toolbar_title);
+        setSupportActionBar(toolbar);
+        toolbarTitle.setText(Title);
+    }
+
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
@@ -30,15 +40,19 @@ public class MainActivity extends AppCompatActivity {
                     switch(menuItem.getItemId()){
                         case R.id.action_home:
                             selectedFragment = new home();
+                            changeToolbarText("Home");
                             break;
                         case R.id.action_events:
                             selectedFragment = new events();
+                            changeToolbarText("Events");
                             break;
                         case R.id.action_blog:
                             selectedFragment = new blog();
+                            changeToolbarText("Blog");
                             break;
                         case R.id.action_more:
                             selectedFragment = new more();
+                            changeToolbarText("More");
                             break;
                     }
 
