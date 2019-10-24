4db2f286b9ad5e685ae6ec08addd99fae8c68820
KendallRConley <kendall.conley@uky.edu>
2019-10-24 18:43:28 -0400

    Implemented rave hour feature. Icon for it and QR is temporary. This also broke toolbar, will fix later.

--- app/src/main/java/com/example/danceblue/MainActivity.java
+++ app/src/main/java/com/example/danceblue/MainActivity.java
@@ -6,12 +6,15 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +25,14 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new home()).commit();
+
+        final ImageButton raveButton = findViewById(R.id.rave_button);
+        raveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.startActivity(new Intent(MainActivity.this, raveHour.class));
+            }
+        });
     }
 
     private void changeToolbarText (String Title){
---
+++ app/src/main/java/com/example/danceblue/raveHour.java
@@ -0,0 +1,41 @@
+package com.example.danceblue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class raveHour extends AppCompatActivity {
+    final Handler handler = new Handler();
+    List<Integer> colors = new ArrayList<>();
+    private int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rave_hour);
+
+        String[] colorsTxt = getApplicationContext().getResources().getStringArray(R.array.colors);
+        for (String s : colorsTxt) {
+            int newColor = Color.parseColor(s);
+            colors.add(newColor);
+        }
+        runnable.run();
+    }
+
+    private Runnable runnable = new Runnable(){
+        @Override
+        public void run(){
+            View background = findViewById(R.id.rave_back);
+            background.setBackgroundColor(colors.get(count));
+            count++;
+            if (count == 3) {count = 0;}
+            handler.postDelayed(runnable, 2000);
+        }
+    };
+}
