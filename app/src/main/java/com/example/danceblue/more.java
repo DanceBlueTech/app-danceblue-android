1ca262f870d5bdb5faa79b7701ef212bb6390560
KendallRConley <kendall.conley@uky.edu>
2019-10-29 14:10:52 -0400

    Fixed layout, added FAQ section to more, but its empty rn

--- app/src/main/java/com/example/danceblue/more.java
+++ app/src/main/java/com/example/danceblue/more.java
@@ -1,5 +1,6 @@
 package com.example.danceblue;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -7,11 +8,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 public class more extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.more, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ImageButton layoutButton = getView().findViewById(R.id.FAQs);
+        layoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), viewFAQ.class);
+                startActivity(intent);
+            }
+        });
     }
 }
