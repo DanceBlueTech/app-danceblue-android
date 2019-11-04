40508beaec6c019938bd1822fe1c137e7eca245f
KendallRConley <kendall.conley@uky.edu>
2019-11-04 13:41:39 -0500

    added contact page. kinda buggy and slow rn, need to improve

--- app/src/main/java/com/example/danceblue/viewContact.java
+++ app/src/main/java/com/example/danceblue/viewContact.java
@@ -3,6 +3,7 @@ package com.example.danceblue;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class viewContact extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class viewContact extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_contact);
+
+        WebView contact = findViewById(R.id.contactViewer);
+        contact.loadUrl("http://www.danceblue.org/meet-the-team/");
     }
 }
